3f7686fac5569c16f685da67006b8bc453fb1f1a
NyverZ <nyverzthurfah@gmaail.com>
2026-01-12 18:58:04 +0800

    y

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  safelist: [
+    {
+      pattern: /dark:/,
+    },
+  ],
   theme: {
     extend: {},
   },
