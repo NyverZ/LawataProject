5f960d470612ed23b5edc3dec97e2214b0914f40
NyverZ <nyverzthurfah@gmaail.com>
2026-01-12 18:36:43 +0800

    main

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "@/components/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider storageKey="theme">
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
       <BrowserRouter>
         <Navbar />
         <App />
